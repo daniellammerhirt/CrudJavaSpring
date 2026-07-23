fd2a74ea7c9e0d49f5e53b5daee93e978a01a811
daniellammerhirt <daniellammer75@gmail.com>
2026-07-23 16:42:19 -0300

    feat: implementa crud de Spring

---
+++ src/main/java/repository/CardapioRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Cardapio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardapioRepository extends JpaRepository<Cardapio, Integer> {
+
+}
---
+++ src/test/java/crudjavaspring/CrudjavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package crudjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudjavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
